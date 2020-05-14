e7a86db1f8e8744efc33de5d49bb695fffa5cb71
Cristian Iacob <iacobtudorcristian@gmail.com>
2020-05-14 12:21:11 +0200

    Ready to work

---
+++ app/menu_datos.js
@@ -0,0 +1,182 @@
+let menu = [
+    {
+        label: 'Introducción',
+        href: 'index.html',
+        icon: '',
+        children: []
+    }, {
+        label: '¿Qué es moc?',
+        href: '#',
+        icon: '',
+        children: [
+            {
+                label: 'Definición',
+                href: '#'
+            },
+            {
+                label: 'ADN marca',
+                href: '#'
+            },
+            {
+                label: 'Estrategia',
+                href: '#'
+            },
+            {
+                label: 'Drivers',
+                href: '#'
+            },
+            {
+                label: 'Narrativa',
+                href: '#'
+            }
+        ]
+    }, {
+        label: 'Definición de la Marca',
+        href: '#',
+        icon: 'twitter',
+        children: [
+            {
+                label: 'Elementos',
+                href: '#'
+            },
+            {
+                label: 'Composición',
+                href: '#'
+            },
+            {
+                label: 'Color',
+                href: 'color.html'
+            },
+            {
+                label: 'Animación',
+                href: '#'
+            }
+        ]
+    }, {
+        label: 'Identidad Visual',
+        href: '#',
+        icon: 'check',
+        children: [
+            {
+                label: 'Tipografía',
+                href: '#'
+            },
+            {
+                label: 'Gama Colores',
+                href: '#'
+            },
+            {
+                label: 'Retícula',
+                href: '#'
+            },
+            {
+                label: 'Estilo Fotográfico',
+                href: '#'
+            },
+            {
+                label: 'Estilo Ilustración',
+                href: '#'
+            },
+            {
+                label: 'Estilo Gráficas',
+                href: '#'
+            }
+        ]
+    },
+    {
+        label: 'Aplicaciones',
+        href: '#',
+        icon: 'check',
+        children: [
+            {
+                label: 'Impresas',
+                href: '#'
+            },
+            {
+                label: 'Digitales',
+                href: '#'
+            },
+            {
+                label: 'Plantillas',
+                href: '#'
+            },
+            {
+                label: 'Video',
+                href: '#'
+            }
+        ]
+    },
+    {
+        label: 'About us',
+        href: '#',
+        icon: 'check',
+        children: []
+    }
+];
+
+
+
+
+
+
+
+
+
+const drawMenuItemContent = (currentMenuItem) => {
+
+    if (currentMenuItem.children.length == 0) {
+        return ``;
+
+    } else {
+        let children = currentMenuItem.children;
+        let output = '';
+
+        for (let i = 0; i < children.length; i++) {
+            let currentMenuSubItem = children[i];
+
+            output += `
+                    <li>
+                        <a href="${currentMenuSubItem.href}">${currentMenuSubItem.label}</a>
+                    </li>
+            `;
+        }
+
+        return output;
+    }
+
+};
+
+
+let dynamicMenu = document.querySelector('#leftMenu');
+dynamicMenu.innerHTML = '';
+
+for (let i = 0; i < menu.length; i++) {
+    let currentMenuItem = menu[i];
+
+    if (currentMenuItem.children.length == 0) {
+        let menuHTML = `
+        <div class="leftMenuItem">
+            <h3><a href="${currentMenuItem.href}">${currentMenuItem.label}</a></h3>
+            </div>
+        `;
+        dynamicMenu.innerHTML += menuHTML;
+
+    }
+
+    else{
+        let menuHTML = `
+        <div class="leftMenuItem">
+             <h3>${currentMenuItem.label}</h3>
+                <ul class="BrandDropdown">
+                    ${drawMenuItemContent(currentMenuItem)}
+                </ul>
+            </div>
+    `;
+    dynamicMenu.innerHTML += menuHTML;
+
+    }
+    
+    
+}
+
+
